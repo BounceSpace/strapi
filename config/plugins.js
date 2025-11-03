8fefb9adfdf57b4011454158ddceb13e762cdf69
Stijn van Lieshout <vanlieshout.stijn@gmail.com>
2025-11-03 22:43:12 +0000

    Add Netlify deployments plugin for automatic builds
    - Install strapi-plugin-netlify-deployments
    - Configure plugin in config/plugins.js
    - Add Netlify environment variables to .env.example
    - Plugin will trigger Netlify builds when content is published

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,16 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  "netlify-deployments": {
+    enabled: true,
+    config: {
+      accessToken: process.env.NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN || env("NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN"),
+      sites: [
+        {
+          name: env("NETLIFY_SITE_NAME") || "bouncespace.co",
+          id: env("NETLIFY_SITE_ID"),
+          buildHook: env("NETLIFY_BUILD_HOOK"),
+          branch: env("NETLIFY_BRANCH") || "master",
+        },
+      ],
+    },
+  },
+});
